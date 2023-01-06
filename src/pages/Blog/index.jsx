0175663fc367255f92d405fb102b573027b74014
vagnermengali <vagmengali2015@gmail.com>
2023-01-06 12:29:54 -0300

    feat: adding development notice to blog page

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -1,5 +1,35 @@
+import { motion } from "framer-motion";
+import { StyledBlogSection } from "./style";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import { FullPage, Slide } from "react-full-page";
+
 function Blog() {
-  return <></>;
+  document.title = "Blog Prey";
+  return (
+    <motion.div
+      initial={{ opacity: 0.3 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <FullPage>
+        <Slide>
+          <Header />
+          <StyledBlogSection>
+            <div className="container">
+              <div className="content">
+                <h1>Page under development...</h1>
+              </div>
+            </div>
+          </StyledBlogSection>
+        </Slide>
+        <Slide>
+          <Footer />
+        </Slide>
+      </FullPage>
+    </motion.div>
+  );
 }
 
 export default Blog;
