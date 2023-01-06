ee260aa0cd10f5a77f4d3e7dfc027a42d9c2c897
vagnermengali <vagmengali2015@gmail.com>
2023-01-06 10:45:11 -0300

    feat: adding initial section

--- src/pages/Home/InitialSection/index.jsx
+++ src/pages/Home/InitialSection/index.jsx
@@ -1,7 +1,27 @@
 import { StyledInitialSection } from "./style";
+import { useNavigate } from "react-router-dom";
+import padclock from "../../../assets/image/Illustrations/Illustration-cyber-3.svg";
 
 function InitialSection() {
-  return <StyledInitialSection></StyledInitialSection>;
+  const navigate = useNavigate();
+  return (
+    <StyledInitialSection id="home">
+      <div className="container">
+        <div className="container-left">
+          <h1>
+            Your Complete <span>Vulnerability</span> Site.
+          </h1>
+          <p>Find the most beautiful collectibles in one place!</p>
+          <button className="btn" onClick={() => navigate("/register")}>
+            Request free access
+          </button>
+        </div>
+        <div className="container-right">
+          <img className="image" src={padclock} alt="padclock" />
+        </div>
+      </div>
+    </StyledInitialSection>
+  );
 }
 
 export default InitialSection;
