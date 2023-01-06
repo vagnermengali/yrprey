86472987d944e158826f91533991a6175e07b30a
vagnermengali <vagmengali2015@gmail.com>
2023-01-06 12:50:02 -0300

    feat: adding collectibles table component

---
+++ src/components/TableCollection/index.jsx
@@ -0,0 +1,128 @@
+import React from "react";
+import { StyledTableCollection } from "./style";
+import { FaCheckCircle, FaEthereum, FaBitcoin, FaMonero } from "react-icons/fa";
+import profileRed from "../../assets/image/cards/potion-red.png";
+import profileBlue from "../../assets/image/cards/potion-blue.png";
+import profileWhite from "../../assets/image/cards/potion-white.png";
+import profileCian from "../../assets/image/cards/potion-cian.png";
+import profileBeige from "../../assets/image/cards/potion-beige.png";
+
+import { SiZcash, SiDash, SiLitecoin } from "react-icons/si";
+const TableCollection = () => {
+  return (
+    <StyledTableCollection>
+      <div className="content-table">
+        <div className="scope-container">
+          <div className="scope-content">
+            <p>Collections</p>
+            <p>Owners</p>
+            <p>Items</p>
+          </div>
+        </div>
+        <ul className="table-content">
+          <li>
+            <div className="card-table">
+              <div className="collection-details">
+                <p>1</p>
+                <div>
+                  <img src={profileRed} alt="img" />
+                </div>
+                <h3>
+                  Prey Warrior Club Redﾠ
+                  <FaCheckCircle className="checked" />
+                </h3>
+                <span>
+                  <FaEthereum className="eth" /> <SiZcash className="zec" />
+                </span>
+              </div>
+              <p className="card-table-static">1</p>
+              <p className="card-table-static">6</p>
+            </div>
+            <button className="btn-top-collections">purchase</button>
+          </li>
+          <li>
+            <div className="card-table">
+              <div className="collection-details">
+                <p>2</p>
+                <div>
+                  <img src={profileBlue} alt="img" />
+                </div>
+                <h3>
+                  Prey Warrior Club Blueﾠ
+                  <FaCheckCircle className="checked" />
+                </h3>
+                <span>
+                  <FaEthereum className="eth" /> <FaBitcoin className="btc" />
+                </span>
+              </div>
+              <p className="card-table-static">1</p>
+              <p className="card-table-static">6</p>
+            </div>
+            <button className="btn-top-collections">purchase</button>
+          </li>
+          <li>
+            <div className="card-table">
+              <div className="collection-details">
+                <p>3</p>
+                <div>
+                  <img src={profileWhite} alt="img" />
+                </div>
+                <h3>
+                  Prey Warrior Club Whiteﾠ
+                  <FaCheckCircle className="checked" />
+                </h3>
+                <span>
+                  <FaEthereum className="eth" /> <FaMonero className="xmr" />
+                </span>
+              </div>
+              <p className="card-table-static">1</p>
+              <p className="card-table-static">6</p>
+            </div>
+            <button className="btn-top-collections">purchase</button>
+          </li>
+          <li>
+            <div className="card-table">
+              <div className="collection-details">
+                <p>4</p>
+                <div className="container-img">
+                  <img src={profileCian} alt="img" />
+                </div>
+                <h3>
+                  Prey Warrior Club Cianﾠ
+                  <FaCheckCircle className="checked" />
+                </h3>
+                <span>
+                  <FaEthereum className="eth" /> <SiLitecoin className="ltc" />
+                </span>
+              </div>
+              <p className="card-table-static">1</p>
+              <p className="card-table-static">6</p>
+            </div>
+            <button className="btn-top-collections">purchase</button>
+          </li>
+          <li>
+            <div className="card-table">
+              <div className="collection-details">
+                <p>5</p>
+                <div>
+                  <img src={profileBeige} alt="img" />
+                </div>
+                <h3>
+                  Prey Warrior Club Beigeﾠ
+                  <FaCheckCircle className="checked" />
+                </h3>
+                <span>
+                  <FaEthereum className="eth" /> <SiDash className="dash" />
+                </span>
+              </div>
+              <p className="card-table-static">1</p>
+              <p className="card-table-static">6</p>
+            </div>
+            <button className="btn-top-collections">purchase</button>
+          </li>
+        </ul>
+      </div>
+    </StyledTableCollection>
+  );
+};
+export default TableCollection;
