e9449fa41469b9b37b3af91631a54942b8ebcbda
vagnermengali <vagmengali2015@gmail.com>
2023-01-06 12:33:05 -0300

    feat: adding development notice to shop page

---
+++ src/pages/Shop/style.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import background_error from "../../assets/image/backgrounds/background-error.svg";
+
+export const StyledShopSection = styled.section`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-image: url(${background_error});
+  background-repeat: no-repeat;
+  background-size: cover;
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 85%;
+  }
+  .content {
+    display: flex;
+    flex-direction: column;
+
+    gap: 30px;
+  }
+  .content h1 {
+    color: var(--white);
+    font-size: 48px;
+    font-weight: 800;
+    line-height: 1.22;
+    letter-spacing: 0;
+    text-transform: none;
+    gap: 30px;
+  }
+`;
