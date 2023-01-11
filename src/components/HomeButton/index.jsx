446f1b6d0e7632dce9b72f7a2ee7d41ceb92d485
Vagner Mengali <101332741+vagnermengali@users.noreply.github.com>
2023-01-11 00:43:42 -0300

    feat: adding animation to button

--- src/components/HomeButton/index.jsx
+++ src/components/HomeButton/index.jsx
@@ -1,18 +1,33 @@
 import { HomeButtonContainer } from "./style";
 import { RxCaretUp } from "react-icons/rx";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import AOS from "aos";
 
 function HomeButton() {
+  const [showButton, setShowButton] = useState(false);
   useEffect(() => {
     AOS.init({ duration: 2000 });
   }, []);
+  useEffect(() => {
+    const handleButtonVisiblity = () => {
+      window.pageYOffset > 150 ? setShowButton(true) : setShowButton(false);
+    };
+    window.addEventListener("scroll", handleButtonVisiblity);
+    return () => {
+      window.removeEventListener("scroll", handleButtonVisiblity);
+    };
+  }, []);
+  const handleScrollTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   return (
-    <a href="#" className="link">
-      <HomeButtonContainer>
-        <RxCaretUp />
-      </HomeButtonContainer>
-    </a>
+      <div>
+        {showButton && (
+          <HomeButtonContainer onClick={handleScrollTop} data-aos="fade">
+            <RxCaretUp />
+          </HomeButtonContainer>
+        )}
+      </div>
   );
 }
 
