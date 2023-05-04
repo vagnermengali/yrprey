b36cf0fb8caa1fc0da99294b60ce083c3627e924
vagnermengali <vagmengali2015@gmail.com>
2023-05-04 19:21:54 -0300

    feat: adding project refactoring for next JS with typescript

--- src/validators/contact.ts
+++ src/validators/contact.ts
@@ -6,7 +6,7 @@ export const formSchema = yup.object().shape({
     .required("Required field")
     .min(3, "Password must be at least 3 characters long")
     .matches(
-      "^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ\\s]+$",
+      /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ\s]+$/,
       "The name must contain only letters"
     ),
   phone: yup
@@ -17,7 +17,7 @@ export const formSchema = yup.object().shape({
     .string()
     .required("Required field")
     .email("Invalid email")
-    .matches("^[a-z0-9\\_.]+@[a-z]+.([a-z]+)$", "Check your email"),
+    .matches(/^[a-z0-9\\_.]+@[a-z]+\.([a-z]+)$/, "Check your email"),
   subject: yup.string().required("Required field"),
   textMessage: yup.string().required("Required field")
   .min(5, "Password must be at least 5 characters long")
