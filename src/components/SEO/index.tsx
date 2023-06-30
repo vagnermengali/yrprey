019f2516f4af9549da9457f02a5c5b495ac7a24b
vagnermengali <vagmengali2015@gmail.com>
2023-06-30 15:59:23 -0300

    feat: add blog desktop complete

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -1,19 +1,41 @@
 import Head from "next/head";
 import logo from "@/assets/image/logos/logo.svg";
+import banner from "@/assets/image/backgrounds/background-banner.png";
 import { ISeo } from "@/interfaces/ISeo/ISeo";
 
 const SEO = ({ title, description }: ISeo) => {
   const logoUrl = logo.src;
+  const bannerUrl = banner.src;
 
-  return (
-    <Head>
-      <title>{`${title}`}</title>
-      <link rel="shortcut icon" href={logoUrl} type="image/x-icon" />
-      <meta name="description" content={description} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-    </Head>
-  );
+    return (
+        <Head>
+            <meta name="author" content="Vagner Mengali" />
+            <meta name="keywords" content="Javascript" />
+            <link rel="fluid-icon" href={logoUrl} title="YRPrey" />
+            <link rel="icon" type="image/svg+xml" href={logoUrl} />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>{`${title}`}</title>
+            <link rel="shortcut icon" href={logoUrl} type="image/x-icon" />
+            <meta name="description" content={description} />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
+            <meta property="og:site_name" content="YRPrey" />
+            <meta property="og:type" content="website" key="ogtype" />
+            <meta property="og:url" content="http://yrprey.com/" />
+            <meta property="og:image:alt" content="YYRPrey is a Brazilian site for security and vulnerability testing." />
+            <meta property="og:image" content="/banner.svg" />
+            <meta name="twitter:title" content="YRPrey" />
+            <meta name="twitter:site" content="@vagnermengali" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:image" content={bannerUrl} />
+            <meta name="twitter:description" content="YYRPrey is a Brazilian site for security and vulnerability testing." />
+            <meta property="al:ios:app_name" content="YRPrey" />
+            <meta property="al:ios:url" content="http://yrprey.com/" />
+            <meta property="al:android:app_name" content="YRPrey" />
+            <meta property="al:android:url" content="http://yrprey.com/" />
+            <link rel="canonical" href="http://yrprey.com/" />
+        </Head>
+    );
 };
 
 export default SEO;
