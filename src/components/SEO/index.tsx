e4b452c8f252e3972e86307c45ed13632f840459
vagnermengali <vagmengali2015@gmail.com>
2023-06-30 15:45:07 -0300

    feat: add blog desktop complete

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -1,12 +1,14 @@
 import Head from "next/head";
-import logo from "@/assets/image/logos/logo.svg"
-import { ISeo } from "@/interfaces/ISeo/ISeo"
+import logo from "@/assets/image/logos/logo.svg";
+import { ISeo } from "@/interfaces/ISeo/ISeo";
 
 const SEO = ({ title, description }: ISeo) => {
+  const logoUrl = logo.src;
+
   return (
     <Head>
       <title>{`${title}`}</title>
-      <link rel="shortcut icon" href={logo} type="image/x-icon" />
+      <link rel="shortcut icon" href={logoUrl} type="image/x-icon" />
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
